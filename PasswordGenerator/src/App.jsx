aef074bf256d7be00e66a5d51ea1df105cd06d5a
Pridhviraj <pridhvi02@gmail.com>
2024-10-11 11:59:25 +0530

    using useRef and useCallback

---
+++ PasswordGenerator/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import BgChanger from './compoenents/bgchanger'
+import PasswordGenerator from './compoenents/PasswordGenerator'
+
+function App() {
+  return (
+  <>
+  {/*  frist proj */}
+  {/* <BgChanger /> */}
+  <PasswordGenerator/>
+  </> 
+  
+  
+  )
+}
+
+export default App
